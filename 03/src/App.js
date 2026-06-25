8fcd11982bab0c204b4f99d7386c3c655b230033
jsh340866 <jsh340866@gmail.com>
2026-06-25 17:47:35 +0900

    0625

---
+++ 03/src/App.js
@@ -0,0 +1,14 @@
+
+import MyComponent from "./03Component/MyComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>COMPONENT TEST</h1>
+      <hr/>
+      <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
